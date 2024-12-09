6aedadefbf7521feb19cd7fcccbf9a1edbcf69e4
Peter André Altier <petera113.pa@gmail.com>
2024-12-09 10:37:51 +0100

    Main

---
+++ script.js
@@ -0,0 +1,11 @@
+function lightBulb(){
+    document.getElementById("light").style.backgroundColor = "yellow";
+    document.getElementById("light").style.textAlign = "center";
+    document.getElementById("light").style.display = "flex";
+    document.getElementById("light").style.justifyContent = "center"; 
+    document.getElementById("light").style.alignItems = "center"; 
+    document.getElementById("light").innerText = "Lyset er på";
+}
+
+
+
